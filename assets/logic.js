ac0ef3c31e53c83af393b7db444278807bb168d0
mskaff <mskaff904@gmail.com>
2021-07-01 20:16:32 -0400

    completed js functionality/logic

--- assets/logic.js
+++ assets/logic.js
@@ -26,18 +26,40 @@ $(document).ready(function () {
 
     function scheduleUpdater () {
         var currentTime = moment().hour();
-        var timeBlock = $("time-block");
+        var timeBlock = $(".time-block");
+
+        console.log(currentTime);
 
         for (var i = 0; i < timeBlock.length; i++) {
             var hourRow = timeBlock[i];
 
-            if (parseInt(hourRow.id.split("-")[0]) < currentTime) {
-                $(hourRow).addClass("past")
+            var updateHourRow = parseInt(hourRow.id.split("hour-")[1]);
+
+            console.log(updateHourRow);
+
+            if (updateHourRow < currentTime) {
+                $(hourRow).addClass("past");
+            } else if (updateHourRow === currentTime) {
+                $(hourRow).removeClass("past");
+                $(hourRow).addClass("present");
+            } else {
+                $(hourRow).addClass("future");
+                $(hourRow).removeClass("past");
+                $$(hourRow).removeClass("present");
             }
         }
-
-
     }
+
+
+    $('#hour-9 .description').val(localStorage.getItem("hour-9"));
+    $('#hour-10 .description').val(localStorage.getItem("hour-10"));
+    $('#hour-11 .description').val(localStorage.getItem("hour-11"));
+    $('#hour-12 .description').val(localStorage.getItem("hour-12"));
+    $('#hour-13 .description').val(localStorage.getItem("hour-13"));
+    $('#hour-14 .description').val(localStorage.getItem("hour-14"));
+    $('#hour-15 .description').val(localStorage.getItem("hour-15"));
+    $('#hour-16 .description').val(localStorage.getItem("hour-16"));
+    $('#hour-17 .description').val(localStorage.getItem("hour-17"));
 
     scheduleUpdater();
     dateTime();
